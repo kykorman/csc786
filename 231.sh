d65768ae55d28b1c7c09a2db2c3c32c8ccb01276
Kyle Korman <36543014+kykorman@users.noreply.github.com>
2020-12-05 04:06:34 -0500

    Add more current versions of benchmarking scripts

--- 231.sh
+++ 231.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -eux
 gcc -L /libc231/glibc/build/install/lib -I /libc231/glibc/build/install/include -Wl,--rpath=/libc231/glibc/build/install/lib -Wl,--dynamic-linker=/libc231/glibc/build/install/lib/ld-linux-x86-64.so.2 -std=c11 -o 2.31.out test.c
-./2.31.out 10000000
+
+rm 2.31.res
+for((i=0;i<100;i++)); do ./2.31.out 10000000 >> 2.31.res; done
+
